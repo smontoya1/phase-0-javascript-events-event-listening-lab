46d63a1c614a55037d3b05047fc4bafbe5333493
Shaun <smontoya0009@gmail.com>
2025-01-25 15:06:34 -0500

    Fixed errors in assignment

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const input = document.getElementById('button');
 
 function addingEventListener() {
     alert('alert!!');
+    input.addEventListener('click', addingEventListener);
 }
 
-input.addEventListener('click', addingEventListener);
+
 
